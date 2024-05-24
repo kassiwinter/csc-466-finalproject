c538dd167f12c007a8bb7081cdde1b8765830aca
Blake Silton <55601729+BSilton26@users.noreply.github.com>
2024-05-24 15:03:11 -0700

    Add new stopwords. Change DocumentCollection to read stopwords from file.

--- src/DocumentClasses/DocumentCollection.java
+++ src/DocumentClasses/DocumentCollection.java
@@ -1,35 +1,20 @@
 package DocumentClasses;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class DocumentCollection implements Serializable {
+
+    private static final String stopWordsPath = "src/DocumentClasses/files/stopwords-en.txt";
 
     /**
      * Maps each document ID to a {@link TextVector}.
      */
     private final HashMap<Integer, TextVector> documents;
 
-    public static String[] noiseWordArray = {"a", "about", "above", "all", "along",
-            "also", "although", "am", "an", "and", "any", "are", "aren't", "as", "at",
-            "be", "because", "been", "but", "by", "can", "cannot", "could", "couldn't",
-            "did", "didn't", "do", "does", "doesn't", "e.g.", "either", "etc", "etc.",
-            "even", "ever", "enough", "for", "from", "further", "get", "gets", "got", "had", "have",
-            "hardly", "has", "hasn't", "having", "he", "hence", "her", "here",
-            "hereby", "herein", "hereof", "hereon", "hereto", "herewith", "him",
-            "his", "how", "however", "i", "i.e.", "if", "in", "into", "it", "it's", "its",
-            "me", "more", "most", "mr", "my", "near", "nor", "now", "no", "not", "or", "on", "of", "onto",
-            "other", "our", "out", "over", "really", "said", "same", "she",
-            "should", "shouldn't", "since", "so", "some", "such",
-            "than", "that", "the", "their", "them", "then", "there", "thereby",
-            "therefore", "therefrom", "therein", "thereof", "thereon", "thereto",
-            "therewith", "these", "they", "this", "those", "through", "thus", "to",
-            "too", "under", "until", "unto", "upon", "us", "very", "was", "wasn't",
-            "we", "were", "what", "when", "where", "whereby", "wherein", "whether",
-            "which", "while", "who", "whom", "whose", "why", "with", "without",
-            "would", "you", "your", "yours", "yes"};
+    public static String[] noiseWordArray = readWordList(stopWordsPath);
 
     /**
      * Reads the file specified as input and uses the data in the file to populate the documents variable.
@@ -164,5 +149,18 @@ public class DocumentCollection implements Serializable {
             }
         }
         return false;
+    }
+
+    private static String[] readWordList(String filepath) {
+        String[] wordList = {};
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filepath));
+            String allText = String.join(" ", lines);
+            wordList = allText.split("\\s+");
+        } catch (IOException e) {
+            System.out.println("An exception occurred while reading the word list.");
+            System.err.println(e.getMessage());
+        }
+        return wordList;
     }
 }

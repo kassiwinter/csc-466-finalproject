6f2e4a4b33316de5a156149572ead2ad93b2a015
Blake Silton <55601729+BSilton26@users.noreply.github.com>
2024-05-28 13:28:02 -0700

    Add overall design TODOs in main

--- src/main/ArticleClassification.java
+++ src/main/ArticleClassification.java
@@ -7,6 +7,18 @@ public class ArticleClassification {
     private static final String dataDirPath = DataProcessor.processedDataDirPath;
 
     public static void main(String[] args) {
+        // TODO: read documents into labeled DocumentCollections
 
+        // TODO: split each DocumentCollection into proportional training/validation/testing sets
+        // These are split into two steps so we can guarantee the amount of left/center/right samples is nearly the same
+        // TODO: combine the respective subsets into overall training/validation/testing sets.
+        //       We can assume that each of these sets will be a DocumentCollection.
+
+        // TODO: normalize each subset w.r.t. the training set.
+
+        // TODO: use the validation set to find the best k value.
+        //       Measuring "best" here is a design decision we will have to make.
+
+        // TODO: use our selected k-value to evaluate with the testing set.
     }
 }

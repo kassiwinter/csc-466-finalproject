174da064f01d0cf7fa1bbde112c966c760262713
Grant <81586842+grant-baer@users.noreply.github.com>
2024-05-29 12:28:09 -0700

    commented to do list in main

--- src/main/ArticleClassification.java
+++ src/main/ArticleClassification.java
@@ -15,10 +15,15 @@ public class ArticleClassification {
     private static final String dataDirPath = DataProcessor.processedDataDirPath;
 
     public static void main(String[] args) {
+        // going into main function, we have processed data for each category at ../data/processed
+
+        // DONE BY BLAKE : for each category in processed data build DocumentCollections,
+        //       put them into one single folder
         HashMap<String, DocumentCollection> labeledDocCollections = new HashMap<>();
         try (DirectoryStream<Path> dataDirStream = Files.newDirectoryStream(Paths.get(dataDirPath))) {
             for (Path classDir : dataDirStream) {
                 labeledDocCollections.put(classDir.getFileName().toString(),
+                        //
                         new DocumentCollection(classDir, "document"));
             }
         } catch (IOException e) {
@@ -26,12 +31,17 @@ public class ArticleClassification {
             e.printStackTrace(System.err);
         }
 
-        // TODO: split each DocumentCollection into proportional training/validation/testing sets
-        // These are split into two steps so we can guarantee the amount of left/center/right samples is nearly the same
-        // TODO: combine the respective subsets into overall training/validation/testing sets.
-        //       We can assume that each of these sets will be a DocumentCollection.
 
-        // TODO: normalize each subset w.r.t. the training set.
+        // TODO LOGAN: split each DocumentCollection into proportional training/validation/testing sets
+            // for each labeled group, pick 70% for training, 10% for valiudation, 20% for testing
+            // this will be a function where we cna choose what % we want for training,val,testing
+
+        // TODO GRANT: normalize each subset with respect to the training set.
+            // this will require us to normalize training set with its self, and then normalize the
+            // validation/testing sets with respect to the tfidf values from the training normalization
+
+
+        // TODO KASSI: Make actual KNN function refrencing Logan's KNN description
 
         // Optional: make a kNearestNeighbors class to house these functionalities.
         // TODO: use the validation set to find the best k value.
